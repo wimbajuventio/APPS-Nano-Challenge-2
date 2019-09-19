92d111e5641805caa7e6130467c9a472816f76ba
wimba juventio <wimbajuventio@yahoo.com>
2019-09-19 09:50:48 +0700

    upload

--- APPS Nano Challenge 2/ViewController.swift
+++ APPS Nano Challenge 2/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import HealthKit
+
 class ViewController: UIViewController
 {
     
@@ -17,21 +18,28 @@ class ViewController: UIViewController
         let waterType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.dietaryWater)!
         return [waterType]
     }
+
     
     override func viewDidLoad()
+        
     {
         
         super.viewDidLoad()
+        addWaterAmountToHealthKit(miliLiters: 10.0)
+
         
+         if HKHealthStore.isHealthDataAvailable() {
+            let healthStore = HKHealthStore()
+            let allTypes = Set([HKObjectType.quantityType(forIdentifier: .dietaryWater)!])
+            healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+                if !success {
+        // Do any additional setup after loading the view.
+                    
+    }
         
-//         if HKHealthStore.isHealthDataAvailable() {
-//            let healthStore = HKHealthStore()
-//            let allTypes = Set([HKObjectType.quantityType(forIdentifier: .dietaryWater)!])
-//            healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
-//                if !success {
-//        // Do any additional setup after loading the view.
-//    }
-        
+            }
+            
+        }
     }
 
     
@@ -49,28 +57,45 @@ class ViewController: UIViewController
         })
     }
     
-    func addWaterAmountToHealthKit(ounces : Double) {
+    @IBAction func diTekan(_ sender: Any) {
+        print("test button")
+        addWaterAmountToHealthKit(miliLiters: 0.1)
+    }
+    
+    func addWaterAmountToHealthKit(miliLiters : Double) {
         // 1
         let quantityType = HKQuantityType.quantityType(forIdentifier: .dietaryWater)
         
         // string value represents US fluid
         // 2
-        let quanitytUnit = HKUnit(from: "fl_oz_us")
-        let quantityAmount = HKQuantity(unit: quanitytUnit, doubleValue: ounces)
+        let quantitytUnit = HKUnit(from: "fl_oz_us")
+        let quantityAmount = HKQuantity(unit: quantitytUnit,doubleValue: miliLiters)
+        
+        
         let now = Date()
         // 3
         let sample = HKQuantitySample(type: quantityType!, quantity: quantityAmount, start: now, end: now)
         let correlationType = HKObjectType.correlationType(forIdentifier: HKCorrelationTypeIdentifier.food)
         // 4
         let waterCorrelationForWaterAmount = HKCorrelation(type: correlationType!, start: now, end: now, objects: [sample])
+        
+        print("waterCorrelationForWaterAmount:\(waterCorrelationForWaterAmount)")
+        
+        
+        
         // Send water intake data to healthStore…aka ‘Health’ app
         // 5
         self.healthStore?.save(waterCorrelationForWaterAmount, withCompletion: { (success, error) in
             if (error != nil) {
                 NSLog("error occurred saving water data")
             }
+            else
+            {
+                print("saved successfully")
+            }
         })
     }
 
 }
+
 
